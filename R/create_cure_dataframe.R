9d3ac641c21b1b4982e528a2f71a207a0a7c947d
Guillermo Basulto <guillermobasulto@gmail.com>
2022-11-04 10:52:58 -0500

    Update function.

--- R/create_cure_dataframe.R
+++ R/create_cure_dataframe.R
@@ -1,14 +1,41 @@
 #' Calculate CURE Dataframe
 #'
-#' @param plot_varname Variable name to be plot. With or without quotes.
-#' @param dataframe A data frame that contains a variable matching \code{plot_varname} and another cone
+#' @param covariate_values name to be plot. With or without quotes.
+#' @param residuals Residuals.
 #'
-#' @return
+#' @return A data frame with five columns: independent variable, residuals,
+#'   cummulative residuals, lower confidence interval limit, and upper
+#'   confidence interval limit.
 #' @export
 #'
 #' @examples
+#' set.seed(2000)
+#'
+#' ## Define parameters
+#' beta <- c(-1, 0.3, 3)
+#'
+#' ## Simulate idependent variables
+#' n <- 900
+#' AADT <- c(runif(n, min = 2000, max = 150000))
+#' nlanes <- sample(x = c(2, 3, 4), size = n, replace = TRUE)
+#' LNAADT <- log(AADT)
+#'
+#' ## Simulate dependent variable
+#' theta <- exp(beta[1] + beta[2] * LNAADT + beta[3] * nlanes)
+#' y <- rpois(n, theta)
+#'
+#' ## Fit model
+#' mod <- glm(y ~ LNAADT + nlanes, family = poisson)
+#'
+#' ## Calculate residuals
+#' res <- residuals(mod)
+#'
+#' ## Calculate CURE plot data
+#' cure_df <- calculate_cure_dataframe(AADT, res)
+#'
+#' head(cure_df)
 calculate_cure_dataframe <- function(covariate_values, residuals){
-  varname <- print(paste(substitute(covariate_values)))
+  varname <- paste(substitute(covariate_values))
   cat("Covariate = ", varname, "\n")
 
   sd_n <- sqrt(sum(residuals^2))
@@ -31,51 +58,57 @@ calculate_cure_dataframe <- function(covariate_values, residuals){
       upper =  1.96 * sd_cure
     )
 
+  ## Assign independent variable its original name
   out |> dplyr::rename_with(~varname, variable)
 }
 
 
-library(ggplot2)
-library(mgcv)
 
-set.seed(2000)
-
-n <- 300
-AADT <- runif(n, min = 2000, max = 100000)
-LNAADT <- log(AADT)
-beta <- c(-1, 0.3)
-
-theta <- exp(beta[1] + beta[2] * LNAADT)
-beta[1]
-beta[2] * LNAADT
-
-fivenum(beta[2] * LNAADT)
-fivenum(theta)
-
-y <- rpois(n, theta)
-mod <- gam(y ~ LNAADT, family = poisson)
-
-res <- residuals(mod)
-coefficients(mod)
-
-plot(res)
-
-anova(mod)
-summary(mod)
-
-df_cure <- calculate_cure_dataframe(AADT, res)
-
-ggplot(df_cure) +
-  geom_line(aes(x = LNAADT, y = cummres), size = 0.5, colour = "#112446") +
-  geom_line(aes(x = LNAADT, y = upper), size = 0.5, colour = "red") +
-  geom_line(aes(x = LNAADT, y = lower), size = 0.5, colour = "red") +
-  labs(x = "LNAADT", y = "Cummulative Residuals") +
-  theme_light()
-
-
-ggplot(df_cure) +
-  geom_line(aes(x = AADT, y = cummres), size = 0.5, colour = "#112446") +
-  geom_line(aes(x = AADT, y = upper), size = 0.5, colour = "red") +
-  geom_line(aes(x = AADT, y = lower), size = 0.5, colour = "red") +
-  labs(x = "AADT", y = "Cummulative Residuals") +
-  theme_light()
+#
+# library(ggplot2)
+# library(mgcv)
+#
+# set.seed(2000)
+#
+# n <- 900
+# AADT <- c(runif(n, min = 2000, max = 150000))
+# nlanes <- sample(x = c(2, 3, 4), size = n, replace = TRUE)
+# LNAADT <- log(AADT)
+# beta <- c(-1, 0.3, 3)
+#
+# theta <- exp(beta[1] + beta[2] * LNAADT + beta[3] * nlanes)
+# beta[1]
+# beta[2] * LNAADT
+#
+# fivenum(beta[2] * LNAADT)
+# fivenum(theta)
+#
+# y <- rpois(n, theta)
+# mod <- glm(y ~ LNAADT + nlanes, family = poisson)
+#
+# res <- residuals(mod)
+# coefficients(mod)
+#
+# plot(res)
+#
+# anova(mod)
+# summary(mod)
+#
+# df_cure <- calculate_cure_dataframe(AADT, res)
+#
+# calculate_cure_dataframe(LNAADT, res) |>
+#   ggplot() +
+#   geom_line(aes(x = LNAADT, y = cummres), size = 0.5, colour = "#112446") +
+#   geom_line(aes(x = LNAADT, y = upper), size = 0.5, colour = "red") +
+#   geom_line(aes(x = LNAADT, y = lower), size = 0.5, colour = "red") +
+#   labs(x = "LNAADT", y = "Cummulative Residuals") +
+#   theme_light()
+#
+#
+# calculate_cure_dataframe(AADT, res) |>
+#   ggplot() +
+#   geom_line(aes(x = AADT, y = cummres), size = 0.5, colour = "#112446") +
+#   geom_line(aes(x = AADT, y = upper), size = 0.5, colour = "red") +
+#   geom_line(aes(x = AADT, y = lower), size = 0.5, colour = "red") +
+#   labs(x = "AADT", y = "Cummulative Residuals") +
+#   theme_light()
