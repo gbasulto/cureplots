2f8b2ec877a108b265baf3f9d6c14b59fd240b51
Guillermo Basulto <guillermobasulto@gmail.com>
2023-08-04 14:28:45 -0500

    Add lnaadt to data documentation.

--- R/data.R
+++ R/data.R
@@ -14,6 +14,7 @@
 #'   \item{AADT}{Annual Average Daily Traffic (AADT). Double.}
 #'   \item{Length}{Segment length in miles. Double.}
 #'   \item{Total_crashes}{Total crashes. Integer.}
+#'   \item{lnaadt}{Natural logarithm of AADT. Double.}
 #'   \item{lnlength}{Natural logarithm of length in miles. Double.}
 #'   \item{speed50}{Indicator of whether the speed limit is 50 mph or greater. Binary.}
 #'   \item{ShouldWidth04}{Indicator of whether the shoulder is 4 feet or wider. Binary.}
