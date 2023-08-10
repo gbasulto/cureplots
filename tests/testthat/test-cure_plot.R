b122d567f082adcd2743dee7052f5f0c5c22ce32
Guillermo Basulto <guillermobasulto@gmail.com>
2023-08-10 15:55:19 -0500

    Add cure_plot_test.

---
+++ tests/testthat/test-cure_plot.R
@@ -0,0 +1,39 @@
+test_that("ggplot object is genererated", {
+  ## basic example code
+
+  set.seed(2000)
+
+  ## Define parameters
+  beta <- c(-1, 0.3, 3)
+
+  ## Simulate independent variables
+  n <- 900
+  AADT <- c(runif(n, min = 2000, max = 150000))
+  nlanes <- sample(x = c(2, 3, 4), size = n, replace = TRUE)
+  LNAADT <- log(AADT)
+
+  ## Simulate dependent variable
+  theta <- exp(beta[1] + beta[2] * LNAADT + beta[3] * nlanes)
+  y <- rpois(n, theta)
+
+  ## Fit model
+  mod <- glm(y ~ LNAADT + nlanes, family = poisson)
+
+  ## Calculate residuals
+  res <- residuals(mod, type = "working")
+
+  ## Calculate CURE plot data
+  cure_df <- calculate_cure_dataframe(AADT, res)
+
+  head(cure_df)
+
+  ## Providing CURE data frame
+  cure_plot(cure_df)
+
+  ## Providing glm object
+  xx <- cure_plot(mod, "LNAADT")$data
+  dims <- dim(xx)
+
+  expect_equal(dims[1], 900)
+  expect_equal(dims[2], 5)
+})
