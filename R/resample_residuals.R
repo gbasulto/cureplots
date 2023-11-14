f9bb676f2b4853a523cba36a6d276313717d8562
Guillermo Basulto <guillermobasulto@gmail.com>
2023-11-14 13:12:05 -0600

    Silence messages and add sample plot.

--- R/resample_residuals.R
+++ R/resample_residuals.R
@@ -8,7 +8,7 @@
 # ## Define parameters.
 # beta <- c(-1, 0.3, 3)
 #
-# ## Simulate idependent variables
+# ## Simulate independent variables
 # n <- 900
 # AADT <- c(runif(n, min = 2000, max = 150000))
 # nlanes <- sample(x = c(2, 3, 4), size = n, replace = TRUE)
@@ -29,47 +29,52 @@
 #
 # resample_residuals <- function(covariate_values, residuals, n_resamples){
 #
+#   ## Extract string with covariate name
+#   varname <- paste(substitute(covariate_values))
+#
 #   idx <- 1:n_resamples
 #
 #   out_list <-
-#     lapply(idx,
-#            \(x) calculate_cure_dataframe(covariate_values, sample(residuals)) |>
-#              transform(sample = x))
+#     lapply(
+#       idx,
+#       \(x) suppressMessages(
+#         calculate_cure_dataframe(
+#           covariate_values,
+#           sample(residuals))) |>
+#         transform(sample = x) |>
+#         dplyr::rename_with(~varname, covariate_values)
+#     )
 #
 #   do.call("rbind", out_list)
 # }
 #
 # resampled_residuals_tbl <- resample_residuals(AADT, res, n_resamples = 3)
 #
+# ggplot(data = cure_df) +
+#   aes(AADT, cumres) +
+#   geom_line(
+#     data = resampled_residuals_tbl,
+#     aes(group = sample),
+#     col = "grey"
+#   ) +
+#   geom_line(color = "darkgreen", linewidth = 0.8) +
+#   geom_line(
+#     aes(y = lower),
+#     color = "magenta",
+#     linetype = "dashed",
+#     linewidth = 0.8) +
+#   geom_line(
+#     aes(y = upper),
+#     color = "blue",
+#     linetype = "dashed",
+#     linewidth = 0.8) +
+#   theme_bw()
 #
-# resampled_residuals_tbl <- resample_residuals(AADT, res, n_resamples = 3) |>
-#   dplyr::mutate(sample = as.factor(sample)) |>
-#   dplyr::arrange(sample, covariate_values)
 #
-# library(tidyverse)
 #
 # cure_plot(cure_df) +
 #   ggplot2::geom_line(
-#     data = resampled_residuals_tbl |> rename(plotcov__ = covariate_values),
+#     data = resampled_residuals_tbl |> rename(plotcov__ = AADT),
 #     ggplot2::aes(group = sample, y = cumres, color = sample),
-#     col = "gray"
+#     col = "grey"
 #     )
-#
-# resampled_residuals_tbl |> as_tibble() |> rename(plotcov__ = covariate_values)
-#
-# n_resamples <- 3
-#
-# library(tidyverse)
-#
-# if (n_resamples > 0) {
-#   resamples_tbl <-
-#     1:n_resamples |>
-#     map_df(\(x)
-#            calculate_cure_dataframe(AADT, sample(res)) |>
-#              transmute(resample = x, AADT, cumres)
-#     )
-# }
-#
-#
-# cure_plot(cure_df) +
-#   geom_line(data = resamples_tbl, aes(x= AADT, y = cumres, group = resample), color = "grey")
